17a1eabf1497a40ede759a00639e4150905606fa
Gabriel <suppershock777@gmail.com>
2024-11-26 11:00:32 -0300

    rendering data on the front

--- resources/views/livewire/list-products.blade.php
+++ resources/views/livewire/list-products.blade.php
@@ -7,101 +7,75 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-full mx-auto">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="text-xl font-semibold p-10 text-gray-900 dark:text-gray-100">
                     {{ __("Produtos Minerados Cadastrado") }}
                     {{-- <div class="flex items-end justify-end"><a href="#">Cadastrar um produto</a></div> --}}
-                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                        <table class="w-full mt-5 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <div class="relative overflow-x-auto shadow-md mt-5 sm:rounded-lg w-full">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Nome do produto
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Tipo do produto
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Descrição
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Nicho
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    URL (opcional)
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    <span class="sr-only">Edit</span>
-                                </th>
-                            </tr>
+                                <tr>
+                                    <th scope="col" class="px-6 py-3 ">
+                                         Nome do produto
+                                        <span class="text-white pl-0.5 text-base"><i class="bi bi-box-seam"></i></span>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Tipo do produto
+                                        <span class="text-white pl-0.5 text-base"><i class="bi bi-basket3"></i></span>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Descrição
+                                        <span class="text-white pl-0.5 text-base"><i class="bi bi-list"></i></span>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Nicho
+                                        <span class="text-white pl-0.5 text-base"><i class="bi bi-clipboard"></i></span>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        URL
+                                        <span class="text-white pl-0.5 text-base"><i class="bi bi-link-45deg"></i></span>
+                                    </th>
+
+                                    {{-- <th scope="col" class="px-6 py-3 pl-36">
+                                        URL
+                                    </th> --}}
+
+                                    <th scope="col" class="px-6 py-3">
+                                        <span class="sr-only">Edit</span>
+                                    </th>
+                                </tr>
                             </thead>
-                        <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Tipo do produto
-                                </th>
-                                <td class="px-6 py-4">
-                                    Silver
-                                </td>
-                                <td class="px-6 py-4">
-                                    Laptop
-                                </td>
-                                <td class="px-6 py-4">
-                                    $2999
-                                </td>
-                                <td class="px-6 py-4">
-                                    $2999
-                                </td>
-                                <td class="px-6 py-4 text-right">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                </td>
-                            </tr>
-
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Descrição
-                                </th>
-                                <td class="px-6 py-4">
-                                    White
-                                </td>
-                                <td class="px-6 py-4">
-                                    Laptop PC
-                                </td>
-                                <td class="px-6 py-4">
-                                    $1999
-                                </td>
-                                <td class="px-6 py-4">
-                                    $1999
-                                </td>
-                                <td class="px-6 py-4 text-right">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                </td>
-                            </tr>
-                            <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Nicho
-                                </th>
-                                <td class="px-6 py-4">
-                                    Black
-                                </td>
-                                <td class="px-6 py-4">
-                                    Accessories
-                                </td>
-                                <td class="px-6 py-4">
-                                    $99
-                                </td>
-                                <td class="px-6 py-4">
-                                    $99
-                                </td>
-                                <td class="px-6 py-4 text-right">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-                                </td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-                </div>
+                            <tbody>
+                                @foreach($products as $product)
+                                <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $product->product_name }}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {{ $product->product_type }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{ $product->description }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{ $product->niche }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{ $product->url }}
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar <i class="bi bi-pencil-square"></i></a>
+                                        <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Deletar <i class="bi bi-trash"></i></a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="mt-5">
+                        {{ $products->links() }}
+                    </div>
             </div>
         </div>
     </div>
